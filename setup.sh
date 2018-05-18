c4bc14e9a512b886a0c7230408b55c91012a14ce
Kalonji Bankole <kkbankol@us.ibm.com>
2018-05-17 20:37:07 -0700

    fixing travis errors

--- setup.sh
+++ setup.sh
@@ -9,13 +9,12 @@ cd MqttWatsonEventProvider/ && cf push
 # TODO, the mqtt-watson app doesn't receover well, the Cloudant Document with ID <uuid>/msgReceived has to be deleted before restarting/redeploying
 cd ..
 sleep 5
-export provider_url=$(cf apps | grep mqtt-watson | awk '{print $6}' | tr -d ',' | xargs echo -n )
+export provider_url="$(cf apps | grep mqtt-watson | awk '{print $6}' | tr -d ',' | xargs echo -n )"
 # Clean up / Deploy again
-# ./uninstall.sh openwhisk.ng.bluemix.net $(wsk property get --auth | awk '{print $3'}) "wsk"
+# ./uninstall.sh openwhisk.ng.bluemix.net $(wsk property get --auth | awk '{print $3}') "wsk"
 # wsk trigger delete subscription-event-trigger
-sleep 1
-./install.sh openwhisk.ng.bluemix.net $(wsk property get --auth | awk '{print $3'}) "wsk" "http://${provider_url}/mqtt-watson"
-sleep 1
+./install.sh openwhisk.ng.bluemix.net "$(wsk property get --auth | awk '{print $3'})" "wsk" "http://${provider_url}/mqtt-watson"
+sleep 5
 echo "Create trigger that'll be invoked each time a message is received at given topic"
 # wsk trigger delete subscription-event-trigger
 # wsk package delete mqtt-watson
@@ -24,7 +23,7 @@ wsk trigger create mqttMsgReceived \
   --param topic "iot-2/type/${IOT_DEVICE_TYPE}/id/${IOT_DEVICE_ID}/evt/fromClient/fmt/json" \
   --param url "ssl://${IOT_ORG}.messaging.internetofthings.ibmcloud.com:8883" \
   --param username "${IOT_API_KEY}" \
-  --param password "${IOT_AUTH_TOKEN}" \
+  --param password "${IOT_APP_AUTH_TOKEN}" \
   --param client "a:${IOT_ORG}:wskmqttsub_$(date +%s)"
 
 echo "Create rule to invoke action when MQTT messages are received"
